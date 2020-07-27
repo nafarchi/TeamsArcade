237e7f2e9792ccee1234766c2cd4bd90badb8ee9
DanielBub <bublildan@gmail.com>
2020-07-27 16:14:03 +0300

    Take port from process

--- Spaceships/server.js
+++ Spaceships/server.js
@@ -68,6 +68,6 @@ io.on('connection', function (socket) {
   });
 });
 
-server.listen(8081, function () {
+server.listen(process.env.PORT, function () {
   console.log(`Listening on ${server.address().port}`);
 });
